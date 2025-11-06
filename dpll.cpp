984e8247da6c474ca1a175cc0f1da4fb3deab1de
Harry Fielding <harry.fielding@student.manchester.ac.uk>
2025-11-06 16:54:28 +0000

    Parse DIMACS CNF from stdin

--- dpll.cpp
+++ dpll.cpp
@@ -12,3 +12,35 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(void) {
+  int num_vars, num_clauses;
+  std::vector<std::vector<int>> clauses;
+
+  std::string literal;
+
+  while (std::cin >> literal) {
+    if (literal == "c") {
+      std::getline(std::cin, literal);
+    } else if (literal == "p") {
+      std::string string_num_vars, string_num_clauses;
+
+      std::cin >> literal >> string_num_vars >> string_num_clauses;
+
+      num_vars = std::stoi(string_num_vars);
+      num_clauses = std::stoi(string_num_clauses);
+    } else {
+      std::vector<int> clause;
+      while (literal != "0") {
+        clause.push_back(std::stoi(literal));
+        std::cin >> literal;
+      }
+      clauses.push_back(clause);
+    }
+  }
+  return 0;
+}
